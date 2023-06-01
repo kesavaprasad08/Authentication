4f9ce19e1c621c03bb7481a1db38e185dd1d5697
kesavaprasad08 <kesav2661998@gmail.com>
2023-06-01 16:02:19 +0530

    manging the token

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useState, useRef } from "react";
+import { useState, useRef,useContext } from "react";
 
 import classes from "./AuthForm.module.css";
+import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, loading] = useState(false);
@@ -52,7 +55,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data.idToken);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         alert(err.message);
